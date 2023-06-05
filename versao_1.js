ecbdbbda97206c10755ef47605cc5a4e722ed072
mariana <marianalais.eng@gmail.com>
2023-06-05 10:15:03 -0300

    Blackjack versão 1.0

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,36 @@
  * 
  * 
  */
+console.log("Boas vindas ao jogo de BlackJack!");
+const novaRodada = confirm("Quer iniciar uma nova rodada?");
+console.log(novaRodada);
+
+const carta1Usuario = comprarCarta();
+const carta2Usuario = comprarCarta();
+const cartasUsuario = carta1Usuario.valor + carta2Usuario.valor;
+
+const carta1Computador = comprarCarta();
+const carta2Computador = comprarCarta();
+const cartasComputador = carta1Computador.valor + carta2Computador.valor;
+
+if (novaRodada) {
+  //inicia o jogo
+  console.log(
+    `Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} - pontuação: ${cartasUsuario}`
+  );
+  console.log(
+    `Computador - cartas: ${carta1Computador.texto} ${carta2Computador.texto} - pontuação: ${cartasComputador}`
+  );
+  if (cartasUsuario > cartasComputador) {
+    console.log("O usuário ganhou!");
+  } else if (cartasUsuario < cartasComputador) {
+    console.log("O computador ganhou!");
+  } else if (cartasUsuario === cartasComputador) {
+    console.log("Empate!");
+  } else {
+    console.log("Jogue outra vez!");
+  }
+} else {
+  //O jogo acaba
+  alert("O jogo acabou");
+}
